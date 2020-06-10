7c3ae805c94e5203f45dd486330d62e2538ae838
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-10 20:25:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def position_taken?(board, index)
  end
 end
 
-def valid_move?
+def valid_move?(board, index)
   if board[index].to_i >= 8
     false
   elsif position_taken?(board, index) == true
